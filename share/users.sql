4744e5c92d404830646a574434238a3e095391f7
Noah Isaacson <noah@nisaacson.com>
2013-07-05 12:23:04 -0600

    added default roles to source sql file

--- share/users.sql
+++ share/users.sql
@@ -8,6 +8,34 @@ SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+--
+-- Name: uuid-ossp; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+
+
+--
+-- Name: EXTENSION "uuid-ossp"; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION "uuid-ossp" IS 'generate universally unique identifiers (UUIDs)';
+
+
 SET search_path = public, pg_catalog;
 
 SET default_tablespace = '';
@@ -15,12 +43,33 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- Name: users; Type: TABLE; Schema: public; Tablespace:
+-- Name: access_tokens; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE access_tokens (
+    token uuid NOT NULL,
+    email text NOT NULL,
+    available boolean
+);
+
+
+--
+-- Name: roles; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE roles (
+    name text NOT NULL,
+    id uuid NOT NULL
+);
+
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE TABLE users (
     id uuid NOT NULL,
-    email text,
+    email text NOT NULL,
     password text,
     confirmed boolean DEFAULT false,
     confirm_token text,
@@ -29,7 +78,42 @@ CREATE TABLE users (
 
 
 --
--- Name: pk_users; Type: CONSTRAINT; Schema: public; Tablespace:
+-- Data for Name: access_tokens; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY access_tokens (token, email, available) FROM stdin;
+\.
+
+
+--
+-- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY roles (name, id) FROM stdin;
+user	345e1294-f094-4e11-b28a-c0067b1a34d6
+staff	9c4d4a3d-6570-44ab-b1b6-400dee40bde5
+admin	95e4fdb4-f1e7-49f5-90fd-12f4ae5187c3
+\.
+
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+COPY users (id, email, password, confirmed, confirm_token, reset_token) FROM stdin;
+\.
+
+
+--
+-- Name: access_tokens_token_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY access_tokens
+    ADD CONSTRAINT access_tokens_token_pkey PRIMARY KEY (token);
+
+
+--
+-- Name: pk_users; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
 --
 
 ALTER TABLE ONLY users
@@ -37,23 +121,67 @@ ALTER TABLE ONLY users
 
 
 --
--- Name: unique_confirm_token; Type: INDEX; Schema: public; Tablespace:
+-- Name: roles_id_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY roles
+    ADD CONSTRAINT roles_id_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: roles_name_unique_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY roles
+    ADD CONSTRAINT roles_name_unique_key UNIQUE (name);
+
+
+--
+-- Name: users_email_unique; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY users
+    ADD CONSTRAINT users_email_unique UNIQUE (email);
+
+
+--
+-- Name: unique_confirm_token; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE UNIQUE INDEX unique_confirm_token ON users USING btree (confirm_token);
 
+
 --
--- Name: unique_reset_token; Type: INDEX; Schema: public; Tablespace:
+-- Name: unique_email_lower_case; Type: INDEX; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE UNIQUE INDEX unique_email_lower_case ON users USING btree (lower(email));
+
+
+--
+-- Name: unique_reset_token; Type: INDEX; Schema: public; Owner: -; Tablespace: 
 --
 
 CREATE UNIQUE INDEX unique_reset_token ON users USING btree (reset_token);
 
 
 --
--- Name: unique_email_lower_case; Type: INDEX; Schema: public; Tablespace:
+-- Name: access_tokens_email_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
-CREATE UNIQUE INDEX unique_email_lower_case ON users USING btree (lower(email));
+ALTER TABLE ONLY access_tokens
+    ADD CONSTRAINT access_tokens_email_fkey FOREIGN KEY (email) REFERENCES users(email) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: public; Type: ACL; Schema: -; Owner: -
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM noah;
+GRANT ALL ON SCHEMA public TO noah;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
 
 --
 -- PostgreSQL database dump complete
